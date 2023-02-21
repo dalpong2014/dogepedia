e5697192b3d9a295876be78fd5ce1a1e9cd6d92e
Ethan Lee <dalpong2014@gmail.com>
2023-02-21 14:16:12 -0500

    pokemon names w/ pagination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
-  return <PokemonList pokemon={pokemon} />;
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setcurretPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon/next"
+  );
+  const [prevPageUrl, setPrevPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon/prev"
+  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+
+    //clean up function. Everytime we make new request, we clean up the last request
+    return () => {
+      cancel();
+    };
+  }, [currentPageUrl]);
+
+  const gotoNextPage = () => {
+    setcurretPageUrl(nextPageUrl);
+  };
+  const gotoPrevPage = () => {
+    setcurretPageUrl(prevPageUrl);
+  };
+
+  if (loading) return "Loading...";
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      />
+    </>
+  );
 }
 
 export default App;
