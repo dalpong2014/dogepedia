c2717794efb236a16b1cfdd6dc84f885e90049a4
Ethan Lee <dalpong2014@gmail.com>
2023-03-05 20:26:19 -0500

    made search bar and deleted dropdown option

--- src/BreedList.js
+++ src/BreedList.js
@@ -4,12 +4,48 @@ import { Select, Option } from "@material-tailwind/react";
 export default function PokemonList({ breeds }) {
   console.log("breed in list", breeds);
   return (
-    <div className="w-36">
-      <Select label="Select a Breed">
-        {breeds.map((eachBreed) => {
-          return <Option key={eachBreed}>{eachBreed}</Option>;
-        })}
-      </Select>
+    <div>
+      <h1>Search for a dog breed</h1>
+      <form>
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-white sr-only dark:text-white"
+        >
+          Search
+        </label>
+        <div class="relative">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-white dark:white"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            class="block w-full p-4 pl-10 text-sm text-white border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-blue-400 dark:border-gray-600 dark: dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search for a dog breed"
+            required
+          ></input>
+          <button
+            type="submit"
+            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
